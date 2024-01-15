2a4a3ae52ee750c2a76e841b9e4f83412ec59339
GFDSA <136776084+GFDSA030@users.noreply.github.com>
2024-01-15 23:45:22 +0900

    修正

--- src/main.cpp
+++ src/main.cpp
@@ -22,20 +22,20 @@ struct user{//プロポ入力
 
 class motor{//モーターチャンネルとピン設定
     public:
-        int nf = 0;//モーターの回転ON/OFF
-        int d = 0;//もとになる数値
-        int c1 = 0;//左前変更値
-        int c2 = 0;//右前変更値
-        int c3 = 0;//左後変更値
-        int c4 = 0;//右後変更値
-        int ch1 = 0;
-        int ch2 = 0;
-        int ch3 = 0;
-        int ch4 = 0;
-        int pin1 = 0;
-        int pin2 = 0;
-        int pin3 = 0;
-        int pin4 = 0;
+        int nf;//モーターの回転ON/OFF
+        int d;//もとになる数値
+        int c1;//左前変更値
+        int c2;//右前変更値
+        int c3;//左後変更値
+        int c4;//右後変更値
+        int ch1;
+        int ch2;
+        int ch3;
+        int ch4;
+        int pin1;
+        int pin2;
+        int pin3;
+        int pin4;
         void rotate();
         motor(int pin_1,int pin_2,int pin_3,int pin_4,int cha1,int cha2,int cha3,int cha4):pin1(pin_1),pin2(pin_2),pin3(pin_3),pin4(pin_4),ch1(cha1),ch2(cha2),ch3(cha3),ch4(cha4){}
         void setup();
