fe533dbd1a237653fac879330989c88ae6d09431
GFDSA <136776084+GFDSA030@users.noreply.github.com>
2024-02-26 16:29:32 +0900

    修正

--- src/controler.h
+++ src/controler.h
@@ -21,6 +21,9 @@ private:
   int c_z;
   int c_turn;
   user ud;//標準
+#ifdef interrupt_on
+  static interrupt i;
+#endif
 };
 
 contloler::contloler(int pin1, int pin2, int pin3, int pin4, int pin5, int pin6, user set_)
@@ -33,7 +36,7 @@ contloler::contloler(int pin1, int pin2, int pin3, int pin4, int pin5, int pin6,
   pin_in6 = pin6;
   set = set_;
 #ifdef interrupt_on
-  static interrupt i(pin_in1,pin_in2,pin_in3,pin_in4,pin_in5,pin_in6,set);
+  i = i(pin_in1,pin_in2,pin_in3,pin_in4,pin_in5,pin_in6,set);
 #endif
 }
 void contloler::setup()
