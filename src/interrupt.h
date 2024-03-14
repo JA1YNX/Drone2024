000b4f470e63e49fa69765f9d8b0e075d2ecce6f
GFDSA <136776084+GFDSA030@users.noreply.github.com>
2024-03-14 10:18:57 +0900

    xcore

--- src/interrupt.h
+++ src/interrupt.h
@@ -12,20 +12,34 @@ private:
     static user set;
     static user read;
     volatile static user count;
+    TaskHandle_t thp[1];//マルチスレッドのタスクハンドル格納用
+    static void xcore();
 };
 
 interrupt::interrupt(int pin1, int pin2, int pin3, int pin4, int pin5, int pin6, user set_)
 {
+
+    /*
     timer = timerBegin(0,80,true);
     timerAttachInterrupt(timer,&interrupt_fun,true);
     timerAlarmWrite(timer,interrupt_clock,true);
     timerAlarmEnable(timer);
+    */
     set = set_;
+    xTaskCreatePinnedToCore(xcore, "Core0a", 4096, NULL, 3, &thp[0], 0); 
 }
 
 user interrupt::out()
 {
     return read;
+}
+
+void interrupt::xcore()
+{
+    while(1)
+    {
+        interrupt_fun();
+    }
 }
 
 void interrupt::interrupt_fun()
