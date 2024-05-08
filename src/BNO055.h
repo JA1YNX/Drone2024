9b45d7955d2606387d30c770b4ae0fb8bdc4a058
GFDSA <136776084+GFDSA030@users.noreply.github.com>
2024-05-08 15:56:37 +0900

    修正

--- src/BNO055.h
+++ src/BNO055.h
@@ -143,10 +143,10 @@ void bno055::get_bno055_data(void)
   //Serial.print(quat.z(), 4);
   //Serial.print("\t\t");
 
-  j.x += (static_cast<int>(accelermetor.x()*10))/10;
-  j.y += (static_cast<int>(accelermetor.y()*10))/10;
+  j.x += (static_cast<int>(accelermetor.x()*10)/10);
+  j.y += (static_cast<int>(accelermetor.y()*10)/10);
   j.z += (static_cast<int>(accelermetor.z()*10)/10)-9.8;
-  j.turn += (static_cast<int>(gyroscope.x()*10))/10;
+  j.turn += (static_cast<int>(gyroscope.x()*10)/10);
 
   //j.x = (j.x/2+euler.x()/2);
   //j.z = (j.y/2+euler.y()/2);
@@ -155,13 +155,13 @@ void bno055::get_bno055_data(void)
 
   #ifdef output
     Serial.print(" DIR_x:");
-    Serial.print((static_cast<int>(accelermetor.x()*10))/10);
+    Serial.print((static_cast<int>(accelermetor.x()*10)/10));
     Serial.print(" DIR_y:");
-    Serial.print((static_cast<int>(accelermetor.y()*10))/10);
+    Serial.print((static_cast<int>(accelermetor.y()*10)/10));
     Serial.print(" DIR_z:");
     Serial.print((static_cast<int>(accelermetor.z()*10)/10)-9.8);
     Serial.print(" DIR_T:");
-    Serial.println((static_cast<int>(gyroscope.x()*10))/10);
+    Serial.println((static_cast<int>(gyroscope.x()*10)/10));
     #endif
   //Serial.println();
 }
