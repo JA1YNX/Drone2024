2f809da428c787f9dff40139cf45814eca7123c6
GFDSA <136776084+GFDSA030@users.noreply.github.com>
2024-05-28 16:40:56 +0900

    実装

--- src/KXR_94.h
+++ src/KXR_94.h
@@ -1,2 +1,30 @@
 #pragma once
 #include "./conf.h"
+
+class KXR_94
+{
+    public:
+    void setup(user);
+    user read();
+    
+    private:
+    user pin;
+};
+
+void KXR_94::setup(user pin_in)
+{
+    pin = pin_in;
+    pinMode(pin_in.x,INPUT);
+    pinMode(pin_in.y,INPUT);
+    pinMode(pin_in.z,INPUT);
+    return;
+}
+
+user KXR_94::read()
+{
+    user ret;
+    ret.x = analogRead(pin.x);
+    ret.y = analogRead(pin.y);
+    ret.z = analogRead(pin.z);
+    return ret;
+}
