fc7152f1ba28cdfb87d66e292feee83f72d0a96a
GFDSA <136776084+GFDSA030@users.noreply.github.com>
2024-07-18 10:41:58 +0900

    5

--- src/conf.h
+++ src/conf.h
@@ -22,7 +22,8 @@
     BluetoothSerial bt;
 #endif
 
-#define read_ 0.01 //analogread倍率
+#define read_ 0.005 //analogread倍率
+//#define read_ 0.01 //analogread倍率
 #define hob 2.0 //ホバリング時センサ倍率
 
 #define puls 200//pwm周波数
