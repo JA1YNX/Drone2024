6ebde110d03af138ee8854a397e561ad8a2fae3d
GFDSA <136776084+GFDSA030@users.noreply.github.com>
2024-07-22 15:05:33 +0900

    setup

--- src/debug.hpp
+++ src/debug.hpp
@@ -3,20 +3,21 @@
 #include "./motor.h"
 #include "./KXR_94.h"
 
+contloler c(user<int>{32, 35, 33, 34});   //T6J   //ch1pin,ch2pin,ch3pin,ch4pin
 void setup(void)
 {
   Serial.begin(9600);
+  c.setup();
 }
 
-contloler c(user<int>{32, 35, 33, 34});   //T6J   //ch1pin,ch2pin,ch3pin,ch4pin
 void loop(void)
 {
     Serial.print("x:");
-    Serial.print((double)pulseIn(32,HIGH));
+    Serial.print((double)pulseIn(32,HIGH,100000));
     Serial.print("y:");
-    Serial.print((double)pulseIn(35,HIGH));
+    Serial.print((double)pulseIn(35,HIGH,100000));
     Serial.print("z:");
-    Serial.print((double)pulseIn(33,HIGH));
+    Serial.print((double)pulseIn(33,HIGH,100000));
     Serial.print("t:");
-    Serial.println((double)pulseIn(34,HIGH));
+    Serial.println((double)pulseIn(34,HIGH,100000));
 }
