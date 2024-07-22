e07ed409ec433790eb6d86eec73c10a225ef315e
GFDSA <136776084+GFDSA030@users.noreply.github.com>
2024-07-22 15:01:34 +0900

    (int)

--- src/debug.hpp
+++ src/debug.hpp
@@ -12,11 +12,11 @@ contloler c(user<int>{32, 35, 33, 34});   //T6J   //ch1pin,ch2pin,ch3pin,ch4pin
 void loop(void)
 {
     Serial.print("x:");
-    Serial.print(pulseIn(32,HIGH,100000));
+    Serial.print((int)pulseIn(32,HIGH,100000));
     Serial.print("y:");
-    Serial.print(pulseIn(35,HIGH,100000));
+    Serial.print((int)pulseIn(35,HIGH,100000));
     Serial.print("z:");
-    Serial.print(pulseIn(33,HIGH,100000));
+    Serial.print((int)pulseIn(33,HIGH,100000));
     Serial.print("t:");
-    Serial.println(pulseIn(34,HIGH,100000));
+    Serial.println((int)pulseIn(34,HIGH,100000));
 }
