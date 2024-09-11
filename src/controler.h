3f36742fc51ef108592a98588a8f93a2e878d062
GFDSA <136776084+GFDSA030@users.noreply.github.com>
2024-09-11 17:51:02 +0900

    8

--- src/controler.h
+++ src/controler.h
@@ -47,7 +47,7 @@ user<int> contloler::read()
 {
   c.x = (static_cast<int>(pulseIn(set.x,HIGH,20000))-ud.x)/10;
   c.y = (static_cast<int>(pulseIn(set.y,HIGH,20000))-ud.y)/10;
-  c.z = (static_cast<int>(pulseIn(set.z,HIGH,20000))-ud.z)/9;
+  c.z = (static_cast<int>(pulseIn(set.z,HIGH,20000))-ud.z)/8;
   c.turn = (static_cast<int>(pulseIn(set.turn,HIGH,20000))-ud.turn)/10;
 #ifdef output
   bt.print("   cx:");
