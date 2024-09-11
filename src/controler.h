78c51bdd67aa995096342e4ff106589e22d25750
GFDSA <136776084+GFDSA030@users.noreply.github.com>
2024-09-11 16:32:55 +0900

    c

--- src/controler.h
+++ src/controler.h
@@ -16,10 +16,7 @@ public:
   user<int> pin();
 private:
   //スタック用変数
-  int c_x;
-  int c_y;
-  int c_z;
-  int c_turn;
+  user<int> c;
   user<int> ud;//標準値
 };
 
@@ -41,32 +38,27 @@ void contloler::setup()
   pinMode(set.y, INPUT);
   pinMode(set.z, INPUT);
   pinMode(set.turn, INPUT);
-  //値リセット
-  c_x = 0;
-  c_y = 0;
-  c_z = 0;
-  c_turn = 0;
-  delay(5000);
-  ud = user<int>{1500,1500,1000,1500};
+  //delay(5000);
+  ud = user<int>{1520,1520,1110,1520};
   return;
 }
 //読んで返す
 user<int> contloler::read()
 {
-  c_x = ((static_cast<int>(pulseIn(set.x,HIGH,15000))-ud.x)*(1.0)/10.0)*0.11*x_;
-  c_y = ((static_cast<int>(pulseIn(set.y,HIGH,15000))-ud.y)*(-1.0)/10.0)*0.11*y_;
-  c_z = ((static_cast<int>(pulseIn(set.z,HIGH,15000))-ud.z)*(1.0)/8.0)*0.13*x_;
-  c_turn = ((static_cast<int>(pulseIn(set.turn,HIGH,15000))-ud.turn)*(-1.0)/10.0)*0.11*t_;
+  c.x = (static_cast<int>(pulseIn(set.x,HIGH,20000))-ud.x);
+  c.y = (static_cast<int>(pulseIn(set.y,HIGH,20000))-ud.y);
+  c.z = (static_cast<int>(pulseIn(set.z,HIGH,20000))-ud.z);
+  c.turn = (static_cast<int>(pulseIn(set.turn,HIGH,20000))-ud.turn);
 #ifdef output
   bt.print("   cx:");
-  bt.print(c_x);
+  bt.print(c.x);
   bt.print("   cy:");
-  bt.print(c_y);
+  bt.print(c.y);
   bt.print("   cz:");
-  bt.print(c_z);
+  bt.print(c.z);
   bt.print("   ct:");
-  bt.print(c_turn);
+  bt.print(c.turn);
   bt.print("     ");
 #endif
-  return user<int>{static_cast<int>(c_x), static_cast<int>(c_y), static_cast<int>(c_z), static_cast<int>(c_turn)};
+  return {c.x,c.y,c.z,c.turn};
 }
