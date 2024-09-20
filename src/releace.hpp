60f8f002f1521bf8aa544b42cd8d0f9341236e24
GFDSA <136776084+GFDSA030@users.noreply.github.com>
2024-09-21 08:20:20 +0900

    comment

--- src/releace.hpp
+++ src/releace.hpp
@@ -4,9 +4,11 @@
 //#include "./KXR_94.h"
 #include "./BNO055.h"
 
+//状態表示用LED
 #define R_pin 16
 #define Y_pin 5
 #define G_pin 19
+//ch5用読み取りピン
 #define PIN_ch5 23
 
 //モーター制御クラスインスタンス化
@@ -44,6 +46,10 @@ void setup(void)
   //回転OFF
   m.nf = 1;//モーターの回転ON
   m.setup();//初期化
+
+  //BNO055
+  sens.setup();
+
   digitalWrite(R_pin,LOW);
   delay(1000);
   m.stop();
@@ -51,22 +57,18 @@ void setup(void)
   //コントローラー初期化
   c.setup();
 
-  //BNO055
-  sens.setup();
-
-  //回転ストップ
-  //m.def = 0;//esc初期化
-
-  //更新
-  //m.rotate();//回転
-  
   digitalWrite(G_pin,HIGH);
+
+  //基本軸待機
   while((pulseIn(PIN_ch5,HIGH,20000)>1500)&&(c.read().z>15))
     digitalWrite(R_pin,HIGH);
   while((pulseIn(PIN_ch5,HIGH,20000)<1500)&&(c.read().z<2))
     digitalWrite(R_pin,LOW);
   while(c.read().z>2);
+
   ledcWrite(Y_pin, 0);
+  
+  //基準角度設定
   sens.update();
   history = j.get();
 }
@@ -74,6 +76,7 @@ void setup(void)
 
 void loop(void)
 {
+  //強制停止
   if(pulseIn(PIN_ch5,HIGH,20000)<1500)
   {
     m.stop();
@@ -83,17 +86,16 @@ void loop(void)
     sens.update();
     history = j.get();
   }
+
   m.nf = 1;
   digitalWrite(R_pin,LOW);
   digitalWrite(G_pin,HIGH);
 
   //プロポの入力取得
-  user<int> u = c.read();//プロポ入力
-  //user j = b.bno_read();
+  user<int> u = c.read();
+  
   ledcWrite(Y_pin,u.z*7);
 
-  //処理に使う変数定義
-  //int x, y, z, turn; //諸々値
   #ifdef output
   bt.print("{   Drone2024:");
   #endif
@@ -105,45 +107,48 @@ void loop(void)
   m.c3 = 0;
   m.c4 = 0;
 
-  if(u.y<0)
+  //プロポ反映
   {
-    m.c1 -= u.y;
-    m.c2 -= u.y;
-  }
-  else
-  {
-    m.c3 += u.y;
-    m.c4 += u.y;
-  }
+    if(u.y<0)
+    {
+      m.c1 -= u.y;
+      m.c2 -= u.y;
+    }
+    else
+    {
+      m.c3 += u.y;
+      m.c4 += u.y;
+    }
 
-  if(u.x<0)
-  {
-    m.c2 -= u.x;
-    m.c4 -= u.x;
-  }
-  else
-  {
-    m.c1 += u.x;
-    m.c3 += u.x;
-  }
+    if(u.x<0)
+    {
+      m.c2 -= u.x;
+      m.c4 -= u.x;
+    }
+    else
+    {
+      m.c1 += u.x;
+      m.c3 += u.x;
+    }
 
-  if(u.turn<0)
-  {
-    m.c2 -= u.turn;
-    m.c3 -= u.turn;
+    if(u.turn<0)
+    {
+      m.c2 -= u.turn;
+      m.c3 -= u.turn;
+    }
+    else
+    {
+      m.c1 += u.turn;
+      m.c4 += u.turn;
+    }
   }
-  else
-  {
-    m.c1 += u.turn;
-    m.c4 += u.turn;
-  }
-
-  //BNO055
-  sens.update();
-
-  user<int> j = sens.get();
+  
   //ジャイロ
   {
+    //BNO055
+    sens.update();
+
+    user<int> j = sens.get();
 
     if(u.x == 0)
     {
@@ -177,8 +182,6 @@ void loop(void)
   m.c2 -= 0;
   m.c3 -= 0;
   m.c4 -= 0;
-
-
 
   //回転数更新
   m.rotate();
