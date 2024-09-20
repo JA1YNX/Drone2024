b1f67ffa85b236adbc7ec0fa66ca64cf9bdf6842
GFDSA <136776084+GFDSA030@users.noreply.github.com>
2024-09-20 17:35:52 +0900

    z

--- src/BNO055.h
+++ src/BNO055.h
@@ -65,7 +65,7 @@ void BNO055::update()
     else{
         data.z = 0;
     }
-    data.x = (int)ang.orientation.x;
+    data.x = (int)ang.orientation.z;
     data.y = (int)ang.orientation.y;
     data.turn = (int)ang.orientation.x;
 }
