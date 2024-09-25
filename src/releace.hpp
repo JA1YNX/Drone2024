87af61684c40015842593ceb073d82335e205969
GFDSA <136776084+GFDSA030@users.noreply.github.com>
2024-09-25 17:48:56 +0900

    do

--- src/releace.hpp
+++ src/releace.hpp
@@ -91,11 +91,11 @@ void loop(void)
     digitalWrite(R_pin,HIGH);
     digitalWrite(G_pin,LOW);
     while((c.read().z>2)||(pulseIn(PIN_ch5,HIGH,20000)<1500))m.stop();
-    while(abs(j.x)>Max_ang||abs(j.y)>Max_ang)
-    {
+    
+    do{
       sens.update();
       j = sens.get();
-    }
+    }while(abs(j.x)>Max_ang||abs(j.y)>Max_ang);
     sens.update();
     history = sens.get().turn;
   }
