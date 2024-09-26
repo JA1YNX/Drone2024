9171bccad6fce59dfcae94c5e8d32251d583ffa8
GFDSA <136776084+GFDSA030@users.noreply.github.com>
2024-09-26 12:51:11 +0900

    20

--- src/releace.hpp
+++ src/releace.hpp
@@ -158,7 +158,7 @@ void loop(void)
   }
 
   //ジャイロ
-  user<int> jj = {(0-std::cos((j.x+3.14*4)/8))*10,(0-std::cos((j.y+3.14*4)/8))*10,0,(j.turn-history)/3};
+  user<int> jj = {(0-std::cos((j.x+3.14*4)/8))*20,(0-std::cos((j.y+3.14*4)/8))*20,0,(j.turn-history)/3};
   {
     //BNO055
     if(u.x == 0)
