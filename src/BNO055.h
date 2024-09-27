4f7a394936774ef85b575a988a8957c170984a18
GFDSA <136776084+GFDSA030@users.noreply.github.com>
2024-09-27 11:28:59 +0900

    +

--- src/BNO055.h
+++ src/BNO055.h
@@ -91,10 +91,10 @@ void BNO055::update()
     data.y = (int)ang.orientation.z;
     data.turn = convert((int)ang.orientation.x*(-1));
 
-    data.x -= defolt.x;
-    data.y -= defolt.y;
-    data.z -= defolt.z;
-    data.turn -= defolt.turn;
+    data.x += defolt.x;
+    data.y += defolt.y;
+    data.z += defolt.z;
+    data.turn += defolt.turn;
 
     return;
 }
