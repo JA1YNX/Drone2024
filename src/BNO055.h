f032567764cda5cd5e205b1f2944f7c87c273ed6
s22114 <user001.tool.user@gmail.com>
2024-10-13 11:24:48 +0900

    bno comment

--- src/BNO055.h
+++ src/BNO055.h
@@ -9,15 +9,16 @@
 #include <Adafruit_BNO055.h>
 #include <utility/imumaths.h>
 
+//BNO055制御用クラス
 class BNO055
 {
     public:
-    user<int> get() const;
-    void setup();
-    void update();
-    sensors_event_t getang() const;
+    user<int> get() const;//読み取った値
+    void setup();//セットアップ
+    void update();//これ実行でメンバ変数が更新される
+    sensors_event_t getang() const;//センサの絶対角度を返す
     private:
-    Adafruit_BNO055 bno = Adafruit_BNO055(55,0x28,&Wire);
+    Adafruit_BNO055 bno = Adafruit_BNO055(55,0x28,&Wire);//本来の制御用クラス
     user<int> data;
     sensors_event_t ang,acc;
     int convert(int);
