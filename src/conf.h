4f6241767a132b24c799714145da7aed613f2169
bluePC <user001.tool.user@gmail.com>
2024-11-26 17:37:36 +0900

    change KP,I,D

--- src/conf.h
+++ src/conf.h
@@ -28,9 +28,9 @@ BluetoothSerial bt;
 #define Max_ang 20 // 機体の最大角度これを超えると強制停止
 
 /// pid
-#define KP 1 // 比例ゲイン
-#define KI 0 // 積分ゲイン
-#define KD 0 // 微分ゲイン
+#define KP 1   // 比例ゲイン
+#define KI 0.1 // 積分ゲイン
+#define KD 0.1 // 微分ゲイン
 
 // とりあえずデータ入れるやつ
 // 分かり易いようにz,y,z,turnってしてるけど実際何でもいい
