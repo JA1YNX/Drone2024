e15543162460c866c048ee744ee902858f993e36
bluePC <user001.tool.user@gmail.com>
2024-12-18 19:15:45 +0900

    fulow

--- src/main.cpp
+++ src/main.cpp
@@ -14,3 +14,30 @@
 #ifndef releace_f
 #include "./debug.hpp"
 #endif
+
+
+
+
+/*
+setup()
+|
+|センサ初期化    sens.setup()
+|
+|コントローラー初期化      c.setup()
+|
+|モーター初期化
+|
+|ステータス初期化
+|
+|
+loop()
+|
+|センサ読み取り
+|
+|コントローラー読み取り
+|
+|pid計算
+|
+|モーターに反映
+
+*/
