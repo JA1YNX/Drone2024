f89affda4f527d58552452e168a78a0d772ea4cd
bluePC <user001.tool.user@gmail.com>
2024-12-24 10:33:18 +0900

    add tmp

---
+++ src/BNO055_tmp.h
@@ -0,0 +1,124 @@
+#ifndef BNO
+#define BNO
+
+#include "./conf.h"
+#include <Arduino.h>
+
+#include <Wire.h>
+#include <Adafruit_Sensor.h>
+#include <Adafruit_BNO055.h>
+#include <utility/imumaths.h>
+
+// BNO055制御用クラス
+template<typename T>
+class BNO055_tmp
+{
+public:
+    /// @brief 読み取った値を返す
+    /// @return 読み取った値
+    user<T> get() const
+    {
+        user<T> ret = data;
+        ret.x -= defolt.x;
+        ret.y -= defolt.y;
+        ret.z -= defolt.z;
+        ret.turn -= defolt.turn;
+    
+        // data.turn = ang.orientation.x;
+        return ret;
+    }
+    /// @brief 加工前の値を返す
+    /// @return 読み取った値
+    user<T> data_get() const
+    {
+        return data;
+    }
+    /// @brief セットアップ
+    void setup()
+    {
+        if (!bno.begin())
+        {
+            while (!bno.begin())
+                ;
+        }
+        delay(500);
+        update();
+        return;
+    }
+    /// @brief 値更新
+    void update()
+    {
+        bno.getEvent(&ang, Adafruit_BNO055::VECTOR_EULER);
+        bno.getEvent(&acc, Adafruit_BNO055::VECTOR_LINEARACCEL);
+        if (abs(acc.acceleration.z) > 0.4)
+        {
+            data.z += acc.acceleration.z;
+        }
+        else
+        {
+            data.z = 0;
+        }
+        data.x = (int)ang.orientation.y * (-1);
+        data.y = (int)ang.orientation.z;
+        data.turn = convert((int)ang.orientation.x * (-1));
+    
+        return;
+    }
+    /// @brief 加工前の角度値を返す
+    /// @return sensors_event_t型
+    sensors_event_t getang() const
+    {
+        return ang;
+    }
+    /// @brief 加工前の加速度
+    /// @return sensors_event_t型
+    sensors_event_t getacc() const
+    {
+        return acc;
+    }
+    /// @brief 初期化時点の誤差指定
+    /// @param d 誤差
+    void setd(user<T> d)
+    {
+        defolt = d;
+    }
+
+private:
+    Adafruit_BNO055 bno = Adafruit_BNO055(55, 0x28, &Wire); // 本来の制御用クラス
+    user<T> data;
+    sensors_event_t ang, acc;
+    T convert(T data_)
+    {
+        T ret = 0;
+        /*
+        if (data<180)
+        {
+            ret = data;
+        }
+        else
+        {
+            ret = data-360;
+        }
+        */
+        if ((hist - data_) > (300))
+        {
+            ret = data_ + 360 * hist2;
+            hist2++;
+        }
+        else if ((hist - data_) < (-300))
+        {
+            ret = data_ - 360 * hist2;
+            hist2--;
+        }
+        else
+        {
+            ret = data_ + 360 * hist2;
+        }
+        hist = data_;
+        return ret;
+    }
+    T hist = 0;
+    T hist2 = 0;
+    user<T> defolt;
+};
+#endif
