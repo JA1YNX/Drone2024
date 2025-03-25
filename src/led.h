4b6366f86205b3de7a8b113c6e0fd703534fe9b8
GFDSA <136776084+GFDSA030@users.noreply.github.com>
2025-03-25 21:21:20 +0900

    extern "C"ブロックの位置を修正し、uled_statusおよびledbin列挙型の定義を整理しました。

--- src/led.h
+++ src/led.h
@@ -1,21 +1,30 @@
 #ifndef LED
 #define LED
 #include "conf.h"
-enum uled_status : int
+#ifdef __cplusplus
+extern "C"
 {
-    ready = 0b011,
-    set = 0b101,
-    wait = 0b001,
-    stop = 0b111,
-    none = 0b000,
-    def = 0b111,
-};
-enum ledbin : int
-{
-    R = 0b100,
-    G = 0b010,
-    Y = 0b001,
-};
-int set_led(uled_status status_, int y_va = 128);
-int setup_led();
+#endif
+    enum uled_status : int
+    {
+        ready = 0b011,
+        set = 0b101,
+        wait = 0b001,
+        stop = 0b111,
+        none = 0b000,
+        def = 0b111,
+    };
+    enum ledbin : int
+    {
+        R = 0b100,
+        G = 0b010,
+        Y = 0b001,
+    };
+    int set_led(uled_status status_, int y_va = 128);
+    int setup_led();
+
+#ifdef __cplusplus
+} // extern "C"
+#endif
+
 #endif
