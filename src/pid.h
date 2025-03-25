2c8e53654be0467517eb36eab80f4a335ad1f991
GFDSA <136776084+GFDSA030@users.noreply.github.com>
2025-03-25 21:22:14 +0900

    extern "C"ブロックの位置を修正し、PIDクラスの定義を整理しました。

--- src/pid.h
+++ src/pid.h
@@ -1,33 +1,44 @@
 #ifndef PID
 #define PID
 #include "conf.h"
-/*
-正直PID制御についてあんまりわかってないけどいろんなサイト見てみてこんな感じかな？って感じで実装した結果
-*/
-namespace PID_F
+
+#ifdef __cplusplus
+extern "C"
 {
+#endif
+
+    /*
+    正直PID制御についてあんまりわかってないけどいろんなサイト見てみてこんな感じかな？って感じで実装した結果
+    */
+    namespace PID_F
+    {
 #define DELTA_T 1
 
-    class Pid
-    {
-    private:
-        double pree = 0;
-        double ie = 0;
+        class Pid
+        {
+        private:
+            double pree = 0;
+            double ie = 0;
 
-    public:
-        /// @brief コンストラクタ
-        /// @param 初期化時点の誤差
-        Pid(double);
-        ~Pid();
-        /// @brief PID計算
-        /// @param y 現在値
-        /// @param r 目標値
-        /// @return 出力
-        double calc(double y, double r);
-        /// @brief リセット
-        /// @param 初期化誤差
-        void reset(double pre = 0);
-    };
+        public:
+            /// @brief コンストラクタ
+            /// @param 初期化時点の誤差
+            Pid(double);
+            ~Pid();
+            /// @brief PID計算
+            /// @param y 現在値
+            /// @param r 目標値
+            /// @return 出力
+            double calc(double y, double r);
+            /// @brief リセット
+            /// @param 初期化誤差
+            void reset(double pre = 0);
+        };
 
-}
+    }
+
+#ifdef __cplusplus
+} // extern "C"
+#endif
+
 #endif
