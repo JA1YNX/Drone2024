ee87f26d82f95972a763298886c0ae63dbed5a02
user-b <user-b@localhost>
2025-05-30 17:56:42 +0900

    constexpr修飾子を削除し、定数の定義を整理しました

--- src/conf.h
+++ src/conf.h
@@ -6,39 +6,39 @@
 #include "log.h"
 
 /// @brief 重力加速度
-static constexpr double gravity = 9.80665;
+constexpr double gravity = 9.80665;
 
 /// @brief 赤LEDのピン番号
-static constexpr int R_pin = 16;
+constexpr int R_pin = 16;
 /// @brief 黄LEDのピン番号
-static constexpr int Y_pin = 5;
+constexpr int Y_pin = 5;
 /// @brief 緑LEDのピン番号
-static constexpr int G_pin = 19;
+constexpr int G_pin = 19;
 /// @brief ch5ピン番号
-static constexpr int PIN_ch5 = 33;
+constexpr int PIN_ch5 = 33;
 
 /// @brief デフォルトコントローラーのピン番号
-static constexpr user<int> UC_PIN = {32, 34, 35, 23};
+constexpr user<int> UC_PIN = {32, 34, 35, 23};
 /// @brief デフォルトモーターのピン番号
-static constexpr user<int> UM_PIN = {25, 26, 27, 14};
+constexpr user<int> UM_PIN = {25, 26, 27, 14};
 /// @brief コントローラーデフォルト値
-static constexpr user<int> UC_DEF = {1526, 1526, 1110, 1470};
+constexpr user<int> UC_DEF = {1526, 1526, 1110, 1470};
 
-static constexpr int puls = 73;      // pwm周波数
-static constexpr int bits = 12;      // ledc分解能
-static constexpr int duty_min = 360; // 最低回転
-static constexpr int duty_low = 380; // 最低回転?
+constexpr int puls = 73;      // pwm周波数
+constexpr int bits = 12;      // ledc分解能
+constexpr int duty_min = 360; // 最低回転
+constexpr int duty_low = 380; // 最低回転?
 // 上に二つは正直わかんないけどこれでうまくいってる
-static constexpr int duty_max = 600; // 最高回転
+constexpr int duty_max = 600; // 最高回転
 
-static constexpr double Max_ang = 15.0; // 機体の最大角度これを超えると強制停止
+constexpr double Max_ang = 15.0; // 機体の最大角度これを超えると強制停止
 
 /// pid
-static constexpr double KP_D = 0.0; // 比例ゲイン
-static constexpr double KI_D = 0.0; // 積分ゲイン
-static constexpr double KD_D = 0.0; // 微分ゲイン
+constexpr double KP_D = 0.0; // 比例ゲイン
+constexpr double KI_D = 0.0; // 積分ゲイン
+constexpr double KD_D = 0.0; // 微分ゲイン
 
-static constexpr double PI_D = 3.14159265358979323846;
+constexpr double PI_D = 3.14159265358979323846;
 
 /// ch5の状態確認
 inline bool check5(int in = pulseIn(PIN_ch5, HIGH, 20000), int n = 1500)
