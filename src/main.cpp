60760f9412fd06650a785a12c42cabcf1e7abddb
user-b <user-b@localhost>
2025-05-30 17:57:13 +0900

    PID関連の条件コンパイルを削除し、コードを整理しました

--- src/main.cpp
+++ src/main.cpp
@@ -1,14 +1,8 @@
 // releaes使うかどうか
 #define releace_f
-// PIDにするかどうか
-#define PID_f
 
 #if defined(releace_f)
-#if defined(PID_f)
 #include "./PID.hpp"
-#else
-#include "./releace.hpp"
-#endif
 #endif
 
 #ifndef releace_f
