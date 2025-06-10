0876cf65884d375a2a50237abb239e36b5a95788
user-b <user-b@localhost>
2025-06-10 18:03:25 +0900

    read関数の戻り値のスケーリングを修正し、各軸の計算を調整しました

--- src/controller.cpp
+++ src/controller.cpp
@@ -33,7 +33,7 @@ user<int> controller::read()
     c.y = static_cast<int>(pulseIn(set.y, HIGH, 20000));
     c.z = static_cast<int>(pulseIn(set.z, HIGH, 20000));
     c.turn = static_cast<int>(pulseIn(set.turn, HIGH, 20000));
-    return {(c.x - ud.x) / 25, (c.y - ud.y) / 25, (c.z - ud.z) / 7, (c.turn - ud.turn) / 25};
+    return {(c.x - ud.x) / 90, (c.y - ud.y) / 90, (c.z - ud.z) * 2, (c.turn - ud.turn) / 50};
 }
 void controller::reset(user<int> dd)
 {
