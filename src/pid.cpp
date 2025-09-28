dc056b4bdc1463a2633ae05451c00529f149e7bd
user-b <user-b@localhost>
2025-09-28 16:07:21 +0900

    関数実装分離

--- src/pid.cpp
+++ src/pid.cpp
@@ -2,84 +2,36 @@
 
 namespace PID_F
 {
-    /*double Pid::calc(double y, double r, double a, double p, double i, double d)
+    double PID::calc(double e, double p, double i, double d, double deriv_meas)
     {
-        y -= abe;         // バイアス補正
-        double e = r - y; // 誤差
+        // 積分
+        integrator += 0.5 * (e + e_prev) * T;
+        if (integrator > integrator_max)
+            integrator = integrator_max;
+        if (integrator < integrator_min)
+            integrator = integrator_min;
 
-        // 積分（台形則）
-        ie += (e)*0.01; // note:制御周期0.01sで固定
+        // PID出力
+        double u = p * e + i * integrator - d * deriv_meas;
 
-        // 微分（角加速度に基づく）
-        double de = -a; // note:角加速度が正ならば角速度が増加しているので、誤差は減少する方向に働く
+        e_prev = e;
+        return u;
+    }
+    double AngleController::calc(double y, double r, double rate,
+                                 double p_angle, double i_angle, double d_angle,
+                                 double p_rate, double i_rate, double d_rate)
+    {
+        // ---- 外側ループ：角度PID ----
+        double e_angle = r - y;
+        // D項は角速度を利用
+        double rate_ref = anglePID.calc(e_angle, p_angle, i_angle, d_angle, rate);
 
-        // 出力
-        double u = p * e + i * ie + d * de;
+        // ---- 内側ループ：角速度PID ----
+        double e_rate = rate_ref - rate;
+        // D項は角加速度が理想だが、ジャイロからは直接取れないので
+        // ここは rate の変化を近似的に使う or LPF で滑らかにする
+        double u = ratePID.calc(e_rate, p_rate, i_rate, d_rate, 0.0);
 
         return u;
     }
-    double Pid::calc(double y, double r, double p, double i, double d)
-    {
-        //
-        // double ret = 0;
-        // double e = r - y;
-        // ret += e * KP;          // P項
-        // ret += (e - pree) * KD; // D項
-        // ie += (e + pree) / 2;
-        // ret += ie * KI; // I項
-
-        // 下記変数は既に与えられているものとする
-        // y       : 現在の出力
-        // r       : 現在の目標値
-        // e_pre   : 前回の誤差
-        // T       : 制御周期
-        // KP,KI,KD: P,I,Dゲイン
-
-        // y -= abe;                      // 絶対誤差を引く
-        static double e_pre = 0; // 前回の誤差
-        // static double T = 0.01;  // 制御周期（秒）//todo:制御周期セット
-        static double T_pre = 1; // 制御周期（秒）//todo:制御周期セット
-        double T_now = 2;
-        double T = T_now - T_pre;
-        T_pre = T_now;
-        T = 1;
-        // static double ie = 0;          // 誤差の積分値
-        // double de = 0;                 // 誤差の微分値
-        // double e;                      // 誤差
-        // double u;                      // 出力値
-        // e = r - y;                     // 誤差を計算
-        // de = (e - e_pre) / T;          // 誤差の微分を近似計算
-        // ie = ie + (e + e_pre) * T / 2; // 誤差の積分を近似計算
-        // u = p * e + i * ie + d * de;   // PID制御の式にそれぞれを代入
-        // return u;
-
-        y -= abe;         // バイアス補正
-        double e = r - y; // 誤差
-
-        // 積分（台形則）
-        ie += (e + e_pre) * T / 2.0;
-
-        // 微分（誤差に基づく）
-        double de = (e - e_pre) / T;
-
-        // 出力
-        double u = p * e + i * ie + d * de;
-
-        // 前回誤差更新
-        e_pre = e;
-
-        return u;
-    }
-    Pid::Pid(double _abe)
-    {
-        abe = _abe;
-    }
-    Pid::~Pid()
-    {
-    }
-    void Pid::reset(double _abe)
-    {
-        ie = 0;
-        abe = _abe;
-    }*/
 }
--- src/pid.h
+++ src/pid.h
@@ -2,39 +2,8 @@
 
 #include "conf.h"
 
-/*
-正直PID制御についてあんまりわかってないけどいろんなサイト見てみてこんな感じかな？って感じで実装した結果
-*/
 namespace PID_F
 {
-    // class Pid
-    // {
-    // private:
-    //     double abe = 0; // Absolute Error
-    //     double ie = 0;
-    //     // double KP = 0;
-    //     // double KI = 0;
-    //     // double KD = 0;
-    // public:
-    //     /// @brief コンストラクタ
-    //     /// @param 初期化時点の誤差
-    //     // Pid(double p, double i, double d, double _abe = 0);
-    //     Pid(double _abe = 0);
-    //     ~Pid();
-    //     /// @brief PID計算
-    //     /// @param y 現在値 角度
-    //     /// @param r 目標値
-    //     /// @param a 現在の角速度
-    //     /// @param p Pゲイン
-    //     /// @param i Iゲイン
-    //     /// @param d Dゲイン
-    //     /// @return 出力
-    //     // double calc(double y, double r, double p, double i, double d);
-    //     double calc(double y, double r, double a, double p, double i, double d);
-    //     /// @brief リセット
-    //     /// @param 初期化誤差
-    //     void reset(double _abe = 0);
-    // };
     class PID
     {
     public:
@@ -50,21 +19,7 @@ namespace PID_F
         /// @param d Dゲイン
         /// @param deriv_meas 微分測定値（角加速度など）
         /// @return 出力
-        double calc(double e, double p, double i, double d, double deriv_meas)
-        {
-            // 積分
-            integrator += 0.5 * (e + e_prev) * T;
-            if (integrator > integrator_max)
-                integrator = integrator_max;
-            if (integrator < integrator_min)
-                integrator = integrator_min;
-
-            // PID出力
-            double u = p * e + i * integrator - d * deriv_meas;
-
-            e_prev = e;
-            return u;
-        }
+        double calc(double e, double p, double i, double d, double deriv_meas);
     };
     class AngleController
     {
@@ -81,20 +36,6 @@ namespace PID_F
         /// @param p_rate,  i_rate,  d_rate  角速度ループPIDゲイン
         double calc(double y, double r, double rate,
                     double p_angle, double i_angle, double d_angle,
-                    double p_rate, double i_rate, double d_rate)
-        {
-            // ---- 外側ループ：角度PID ----
-            double e_angle = r - y;
-            // D項は角速度を利用
-            double rate_ref = anglePID.calc(e_angle, p_angle, i_angle, d_angle, rate);
-
-            // ---- 内側ループ：角速度PID ----
-            double e_rate = rate_ref - rate;
-            // D項は角加速度が理想だが、ジャイロからは直接取れないので
-            // ここは rate の変化を近似的に使う or LPF で滑らかにする
-            double u = ratePID.calc(e_rate, p_rate, i_rate, d_rate, 0.0);
-
-            return u;
-        }
+                    double p_rate, double i_rate, double d_rate);
     };
 }
